82bc346824355840c09f772a1ad803c2af155024
hanif@PC <mhz.mdhanif@gmail.com>
2022-06-06 23:50:29 +0800

    edit code

--- loancalculator.js
+++ loancalculator.js
@@ -4,7 +4,7 @@ function computeLoan() {
     var months = document.getElementById('months').value;
     var interest = (amount * (interest_rate * .01)) / months;
     var payment = ((amount / months) + interest).toFixed(2);
-    payment = payment.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",";
+    payment = payment.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 
         document.getElementById('payment').innerHTML = "Monthly Payment = RM" + payment;
 
